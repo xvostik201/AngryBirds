d3354974a2c5eea0b4e5b91a6609e0212c277426
xvostik201 <zkostyutkin2004@gmail.com>
2025-06-24 20:23:01 +0300

    Start slingshot script

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [SerializeField] private LineRenderer[] _lineRenderers;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
