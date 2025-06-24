ce6611ca1d35836deb4bf2796f3177f34ce1935e
xvostik201 <zkostyutkin2004@gmail.com>
2025-06-25 00:12:56 +0300

    Interface for destroy objects

---
+++ Assets/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class Destructible : MonoBehaviour, IDestructible
+{
+    [SerializeField] protected float _deathSpeedThreshold = 2f;
+    protected Rigidbody2D _rb;
+
+    protected virtual void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<IDestructible>(out var other))
+            other.Die();
+
+        float relSpeed = collision.relativeVelocity.magnitude;
+        if (relSpeed > _deathSpeedThreshold)
+            Die();
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Pig.cs
+++ Assets/Pig.cs
@@ -1,18 +1,9 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
+public class Pig : Destructible
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
